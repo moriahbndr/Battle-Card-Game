c573cdc4beb91f186b3152c070476e815476acca
agemtes2 <agemtes2@students.towson.edu>
2025-05-01 23:04:03 -0400

    Add files via upload

---
+++ Battle Card Game/AIGameView.swift
@@ -0,0 +1,20 @@
+//
+//  AIGameView.swift
+//  Battle Card Game
+//
+//  Created by Antikot Gemtessa on 5/1/25.
+//
+
+import SwiftUI
+
+struct AIGameView: View {
+    var body: some View {
+        Text("AI Game Started!")
+            .font(.largeTitle)
+            .fontWeight(.bold)
+            .padding()
+    }
+}
+#Preview {
+    AIGameView()
+}
